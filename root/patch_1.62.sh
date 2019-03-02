91b8636b8509d55c70e2f3dfdf4aa502b87e540f
Yves Schumann <y.schumann@yetnet.ch>
2019-03-02 20:01:34 +0100

    Fixed file permission

--- root/patch_1.62.sh
+++ root/patch_1.62.sh
@@ -7,6 +7,6 @@ echo "Copy patched version to /usr/local/edomi/main/queuecmd/"
 cp /root/patch_1.62/cmd1.php /usr/local/edomi/main/queuecmd/
 
 echo "Fixing permissions"
-chmod +777 /usr/local/edomi/main/queuecmd/cmd1.php
+chmod 777 /usr/local/edomi/main/queuecmd/cmd1.php
 
 echo "Done"
